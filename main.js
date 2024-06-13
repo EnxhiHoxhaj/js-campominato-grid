305038fc1aa412a208d7820142bddb7546c2fcf8
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-13 17:14:49 +0200

    Feat: add comments and js logic diagram

---
+++ main.js
@@ -0,0 +1,10 @@
+////gestione del clicK che genererà la griglia
+    //tramite un ciclo che mi  genera 100 caselle
+    //tramite un ciclo genero un array di numeri
+    // tramite un ciclo inserisco i numeri neglle caselle in modo progressivo da 1 a 100
+    // creo una classe per nascondere nascondere i numeri
+
+//// gestione delle evento click
+    //colorare le caselle di azzurro tramite l'aggiunta di una classe
+    // creo una classe per far stampare i numeri in console
+
