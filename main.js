128c1283e5a0e24cf2f1edeca1c9c7f2ea3627d9
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-14 18:04:24 +0200

    Feat: new Diagram

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
 // richiamo il contenitore dal DOM
 let campoMinato = document.querySelector(".campo");
 let generateCampo = document.querySelector("button");
+let introductioGame = document.querySelector (".intro_play")
 //gestione del clicK che genererà la griglia
 generateCampo.addEventListener ("click",
     function(){ //tramite un ciclo che mi  genera 100 caselle
-        let bombSquare;
         for (i = 1; i <= 100; i++) {
-            let number = i;
+
+            //creiamo i div rappresenta<nti le caselle
             const bombSquare = document.createElement("div");
 
+            // assegnamo la classe "square" e "show_element"
 
+            // assegnamo a l'elemento introduzioneGioco e al bottone la classe "hidden"
             
+            //assegnamo un numero ad ogni casella e stampiamo in pagina il numero
+
+            // creare un click sulle caselle
+
+            // se cliccate prendono la classe "sky_blu"
+
+            // stampare in console il numero
+
+        }
+    }
+)
+
+
+
+
             /*campoMinato.innerHTML += bombSquare;
             campoMinato.classList.add("show_element");
             introductioGame.classList.add("hidden");
@@ -22,10 +40,4 @@ generateCampo.addEventListener ("click",
                     bombSquare.classList.toggle("sky_blu");
                     console.log(i);
                 }
-            )
-        }
-    }
-)
-
-
-
+            )*/
