30c64069b8693ff04b5fafd3026ba398f7c460b2
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-14 10:23:30 +0200

    fix: hidden element

--- main.js
+++ main.js
@@ -12,16 +12,16 @@ generateCampo.addEventListener ("click",
         }
         campoMinato.classList.add("show_element");
         introductioGame.classList.add("hidden");
+        generateCampo.classList.add("hidden");
     }
-    
 )
 
-//for (i= 0 , 1 <= 99; i++) {}
+// al click di ogni singola casella essa si colora di azzurro
+// gestione delle evento click
+//colorare le caselle di azzurro tramite l'aggiunta di una classe
+    // creo una classe per far stampare i numeri in console
+
+    //for (i= 0 , 1 <= 99; i++) {}
     //tramite un ciclo genero un array di numeri
     // tramite un ciclo inserisco i numeri neglle caselle in modo progressivo da 1 a 100
     // creo una classe per nascondere nascondere i numeri
-
-//// gestione delle evento click
-    //colorare le caselle di azzurro tramite l'aggiunta di una classe
-    // creo una classe per far stampare i numeri in console
-
