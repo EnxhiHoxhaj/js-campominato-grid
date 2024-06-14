1a3aa820ae7e54e8b86911e39e33f2ae83004234
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-14 17:51:48 +0200

    feat: second click problem

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
 // richiamo il contenitore dal DOM
 let campoMinato = document.querySelector(".campo");
-let introductioGame = document.querySelector(".intro_play")
 let generateCampo = document.querySelector("button");
 //gestione del clicK che genererà la griglia
 generateCampo.addEventListener ("click",
     function(){ //tramite un ciclo che mi  genera 100 caselle
-        for (i = 0; i <= 99; i++) {
-            let bombSquare = `<div class="square"></div>`;
-            campoMinato.innerHTML += bombSquare;
-            console.log(bombSquare);
+        let bombSquare;
+        for (i = 1; i <= 100; i++) {
+            let number = i;
+            let bombSquare = `<div class="square"> ${number}</div>`;
+            campoMinato.innerHTML = bombSquare;
+            campoMinato.classList.add("show_element");
+            introductioGame.classList.add("hidden");
+            generateCampo.classList.add("hidden");
+
+            // gestione delle evento click al click di ogni singola casella essa si colora di azzurro
+            bombSquare.addEventListener ("click",
+                function() { //colorare le caselle di azzurro e stampa in console il numero
+                    bombSquare.classList.toggle("sky_blu");
+                    console.log(i);
+                }
+            )
         }
-        campoMinato.classList.add("show_element");
-        introductioGame.classList.add("hidden");
-        generateCampo.classList.add("hidden");
     }
 )
 
-// al click di ogni singola casella essa si colora di azzurro
-// gestione delle evento click
-//colorare le caselle di azzurro tramite l'aggiunta di una classe
-    // creo una classe per far stampare i numeri in console
 
-    //for (i= 0 , 1 <= 99; i++) {}
-    //tramite un ciclo genero un array di numeri
-    // tramite un ciclo inserisco i numeri neglle caselle in modo progressivo da 1 a 100
-    // creo una classe per nascondere nascondere i numeri
+
